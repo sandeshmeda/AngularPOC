cd8853d1909f43d779c9a251b8b46afa456ed938
Sandesh Meda <sandeshmeda@gmail.com>
2016-04-13 15:18:47 -0500

    Adding forecast and weather controllers

--- app/forecast/forecast.ctrl.js
+++ app/forecast/forecast.ctrl.js
@@ -1,6 +1,24 @@
 ﻿(function() {
 
-  angular.module('app.forecast').controller('Forecast', function($scope) {
+  angular.module('app.forecast').controller('Forecast', function ($scope, weatherSvc, $routeParams) {
+
+    $scope.forecast = null;
+    $scope.getTime = getTime;
+
+    if ($routeParams.id !== null)
+      getForecast($routeParams.id);
+
+
+    function getForecast(id) {
+      weatherSvc.getForecast(id).then(function(data) {
+        console.log(data);
+        $scope.forecast = data;
+      });
+    }
+
+    function getTime(dt) {
+      return new Date(dt * 1000);
+    }
 
   });
 
--- app/weather/weather.ctrl.js
+++ app/weather/weather.ctrl.js
@@ -1,7 +1,26 @@
 ﻿(function() {
 
   angular.module('app.weather')
-    .controller('Weather', function($scope) {
+    .controller('Weather', function($scope, $routeParams, weatherSvc) {
+
+      $scope.details = null;
+      $scope.getTime = getTime;
+
+      var id = null;
+
+      if ($routeParams.id !== undefined)
+        id = $routeParams.id;
+
+      weatherSvc.getDetail(id).then(function(data) {
+        console.log(data);
+        $scope.details = data;
+      }, function(err) {
+        console.log(err);
+      });
+
+      function getTime(dt) {
+        return new Date(dt * 1000);
+      }
 
     });
 
